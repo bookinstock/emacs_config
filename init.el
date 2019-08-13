69a48f429206833c97bd7b000d120fa3e885b1a9
Wende Lu <wendelu1991@gmail.com>
2019-08-13 19:15:46 +0800

    add .

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+
 (require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
@@ -30,7 +31,9 @@ There are two things you can do about this warning:
  '(custom-safe-themes
    (quote
     ("274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" default)))
- '(package-selected-packages (quote (elixir-mode dracula-theme magit))))
+ '(package-selected-packages
+   (quote
+    (exunit dap-mode lsp-mode eglot counsel ivy flx-ido projectile elixir-mode dracula-theme magit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -51,9 +54,39 @@ There are two things you can do about this warning:
 (set-face-foreground 'font-lock-comment-face "gray")
 (set-face-foreground 'font-lock-doc-face "gray")
 
-;; `load-file` load the lisp file named <file>.
-;; `eval-buffer` execute current buffer as lisp code.
+;; disable auto-save and auto-backup
+(setq make-save-default nil)
+(setq make-backup-files nil)
 
+;; eval buffer and load file
+;;(global-set-key (kbd "C-c b") 'eval-buffer)
+;;(global-set-key (kbd "C-c f") 'load-file)
 
+;; line number
+;; default 'goto-line command -> kbd "s l"
+;;(global-set-key (kbd "C-c l") 'display-line-numbers-mode)
 
+;; projectile
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+;;(setq projectile-enable-caching t)
+;;(setq projectile-project-search-path '("~/Workspace/"))
 
+;; flx-ido
+;;(require 'flx-ido)
+;;(ido-mode 1)
+;;(ido-everywhere 1)
+;;(setq ido-enable-flex-matching t)
+
+;; ruby-mode
+;;(setq ruby-insert-encoding-magic-comment nil)
+
+;; elixir;;
+;;(require 'eglot)
+;;(add-to-list
+;; 'eglot-server-programs
+;; '(elixir-mode "~/xxxx.emacs.d/elixir-ls/release/language_server.sh"))
+
+;;(add-hook 'elixir-mode-hook 'eglot-ensure)
+
+(add-hook 'org-mode-hook 'toggle-truncate-lines) 
